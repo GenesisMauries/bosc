dec6a3de12207ac328856ae987764eed6cb69da5
GenesisMauries <exodo_54@hotmail.com>
2019-03-04 11:32:45 -0600

    Updates

---
+++ precache-manifest.e1f36e373db47dacf7070d0f1d1db59e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bosc/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d90f8bbd6b7ec4db01a6",
+    "url": "/bosc/static/js/runtime~main.d90f8bbd.js"
+  },
+  {
+    "revision": "4764d0b6cda2d50cf063",
+    "url": "/bosc/static/js/main.4764d0b6.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/bosc/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "4764d0b6cda2d50cf063",
+    "url": "/bosc/static/css/main.31b40a87.chunk.css"
+  },
+  {
+    "revision": "862c3fea028cd20ced8a53db15416afc",
+    "url": "/bosc/index.html"
+  }
+];
